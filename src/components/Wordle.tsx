65aacc53663383967e65c518ad1515b4c0d9c6b6
Mario Miličević <mmilic05@fesb.hr>
2025-04-07 22:24:03 +0200

    Keyboard supression p3

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -195,6 +195,7 @@ export default function Wordle() {
                 onKeyDown={handleKeyPress}
                 autoFocus
                 onClick={(e) => e.preventDefault()}
+                tabIndex={-1}
             />
 
             <Grid
